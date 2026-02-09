4610f3deb92741cc278a7b850bbf9cbc8262650d
aryankumar <70devirkt@gmail.com>
2026-02-10 03:08:45 +0530

    remove pr folder

--- pr/src/app.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const app=express();
-const cookieParser=require("cookie-parser");
-const authRouter=require("../routes/user.route.js");
-app.use(cookieParser());
-app.use(express.json());
-app.use("/api/auth",authRouter);
-module.exports=app;
